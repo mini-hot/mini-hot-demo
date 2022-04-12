71fde94bd07a4149e379bd497d499fa716c294e9
wuxx <wuxiaoxi924@pingan.com.cn>
2022-04-12 20:32:33 +0800

    feat(cli): 动态加载的模块分包至dist/remote目录下

--- config/plugins/webpack-plugin-mini-remote-chunk.js
+++ config/plugins/webpack-plugin-mini-remote-chunk.js
@@ -1,4 +1,5 @@
 const fse = require('fs-extra')
+const SplitChunksPlugin = require('webpack/lib/optimize/SplitChunksPlugin')
 const path = require('path')
 
 const PLUGIN_NAME = 'miniRemoteChunkPlugin'
@@ -7,47 +8,58 @@ function isDynamicDep(dep) {
     if (!dep || !dep.type) {
         return false
     }
-    return dep.type.startsWith('import()');
-}
-  
-function isEntryDep(dep) {
-  return dep.type === 'single entry' || dep.type === 'multi entry';
+    return dep.type.startsWith('import()')
 }
 
 function getModuleId(module) {
-    return module.userRequest
+    return module.userRequest || module._identifier
 }
 
-class MiniRemoteChunkPlugin {
-    dynamicDeps = new Set()
-    dynamicDepsMap = new Map()
+class MiniRemoteChunkPlugin extends SplitChunksPlugin {
+    dynamicModules = new Set()
+    options = null
 
     constructor(o) {
-        console.log('constructor : ', o)
+        super(o)
     }
 
     apply(compiler) {
-        compiler.hooks.compilation.tap(PLUGIN_NAME, (compilation) => {
-            compilation.hooks.finishModules.tap(PLUGIN_NAME, (modules) => {
-                modules.forEach(module => {
-                    const { reasons = [] } = module
-                    const moduleId = getModuleId(module)
-                    const isDynamic = reasons.every(reason => {
-                        return isDynamicDep(reason.dependency)
-                    })
-                    if (isDynamic) {
-                        this.dynamicDeps.add(moduleId)
-                        this.dynamicDepsMap.set(moduleId, module)
-                    }
-                })
+        super.apply(compiler)
+        compiler.hooks.compilation.tap(PLUGIN_NAME, compilation => {
+            compilation.hooks.finishModules.tap(PLUGIN_NAME, this.collectDynamicModules)
+            compilation.hooks.optimizeChunks.tap(PLUGIN_NAME, chunks => {
+                let _options = compiler.options.optimization.splitChunks
+                _options.cacheGroups = this.getDynamicChunkCacheGroups(_options.cacheGroups)
+                this.options = SplitChunksPlugin.normalizeOptions(_options)
             })
-        })
-        compiler.hooks.afterCompile.tap(PLUGIN_NAME, () => {
-            return true
         })
     }
 
-    collectDynamicModules(modules) {}
+    collectDynamicModules = (modules) => {
+        modules.forEach(module => {
+            const { reasons = [] } = module
+            const moduleId = getModuleId(module)
+            const isDynamic = reasons.every(reason => isDynamicDep(reason.dependency))
+            if (isDynamic) {
+                this.dynamicModules.add(moduleId)
+            }
+        })
+    }
+
+    getDynamicChunkCacheGroups = (initialCacheGroups = {}) => {
+        return Array.from(this.dynamicModules).reduce((cacheGroups, moduleId) => {
+            const filename = path.basename(moduleId).split('.')[0]
+            cacheGroups[filename] = {
+                name: `/remote/${filename}`,
+                test: module => {
+                    return getModuleId(module) === moduleId
+                },
+                minChunks: 1,
+                priority: 10000,
+            }
+            return cacheGroups
+        }, initialCacheGroups)
+    }
 }
 
 module.exports = {
