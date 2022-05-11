f35bdde133367b582be11ca48db17cc059bd4998
wuchangming <wuchangmingnice@163.com>
2022-05-11 16:41:18 +0800

    chore: update mini-hot version

--- src/pages/dynamic-app/routes.ts
+++ src/pages/dynamic-app/routes.ts
@@ -3,10 +3,12 @@ import { createRemoteAppRoutes } from '@mini-hot/taro'
 export default createRemoteAppRoutes([
     {
         path: '/PageA/:code',
-        getPage: () => import('./PageA'),
+        // PageA 不继续分块
+        getPage: async () => require('./PageA'),
     },
     {
         path: '/PageB',
+        // PageB 继续分块
         getPage: () => import('./PageB'),
     },
 ])
